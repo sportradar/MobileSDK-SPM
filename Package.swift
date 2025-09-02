29c79a6850257b801859dd4e37f4cb1171449040
Gitlab CI Network <network@sportradar.com>
2025-09-02 10:14:19 +0200

    SRSDK 3.9.0 release

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 
-
 // swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
@@ -13,27 +12,15 @@ let package = Package(
     products: [
         .library(
             name: "SRSDK",
-            targets: ["SRSDKTarget"]
+            targets: ["SRSDK"]
         ),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .exact("5.10.2")),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "SRSDK",
-            url: "https://artifacts.mdp.sportradar.com/apple/mobile-sdk-swift/0ca9f7b6/release/SRSDK_3.9.0.xcframework.zip",
-            checksum: "413c1848f757e061832406972d87d610565a7a7e07eec743879c6da1694debab"
-        ),
-        .target(
-            name: "SRSDKTarget",
-            dependencies: [
-                .target(name: "SRSDK"),
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "Logging", package: "swift-log")
-            ],
-            path: "SRSDKTarget")
+            url: "https://artifacts.mdp.sportradar.com/apple/mobile-sdk-swift/7069d175/release/SRSDK_3.9.0.xcframework.zip",
+            checksum: "77685cd55214c619d68e39816fa90b0f9ca7c7e69f26b205c4565aad454e1d37"
+        )
     ]
 )
 
